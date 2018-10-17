edae13150bf42e7835e10d4159eabb2e3b9bb627
gorilla <thisisntlongonlytomakelifehard@gmail.com>
2018-10-17 14:43:03 -0700

    added a socket client

--- app/src/main/java/com/android/admin/uber_guard/Act2Activity.java
+++ app/src/main/java/com/android/admin/uber_guard/Act2Activity.java
@@ -30,7 +30,23 @@ import android.util.Log;
 import android.widget.TextView;
 
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+//REBEL SAFE WALK 1 
 
 public class Act2Activity extends AppCompatActivity {
 
@@ -51,6 +67,26 @@ public class Act2Activity extends AppCompatActivity {
 
     }
 }
+
+class SendHttpString extends AsyncTask<String, Void, String>{
+    private Exception exception;
+
+    protected String doInBackground(String... url){
+
+        String result = "HI";
+
+        return result;
+
+    }
+
+    protected void onPostExecute( String result  ){
+        /*Button b = (Button) findViewById(R.id.buttonHttp    );
+        b.setText(result);*/
+
+    }
+}
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -132,5 +168,51 @@ public class Act2Activity extends AppCompatActivity {
         }
         return result;
     }
+
+    private void sendMessage(final String msg) {
+
+        final Handler handler = new Handler();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    //Replace below IP with the IP of that device in which server socket open.
+                    //If you change port then change the port number in the server side code also.
+                    Socket s = new Socket("xxx.xxx.xxx.xxx", 9002);
+
+                    OutputStream out = s.getOutputStream();
+
+                    PrintWriter output = new PrintWriter(out);
+
+                    output.println(msg);
+                    output.flush();
+                    BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    final String st = input.readLine();
+
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //do sometihng with st
+                            /*
+                            String s = mTextViewReplyFromServer.getText().toString();
+                            if (st.trim().length() != 0)
+                                mTextViewReplyFromServer.setText(s + "\nFrom Server : " + st);*/
+                        }
+                    });
+
+                    output.close();
+                    out.close();
+                    s.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+
 
 }
